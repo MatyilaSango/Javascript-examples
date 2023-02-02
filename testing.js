be8628aeccb7262e041dd9623b8031f9bd2a85a8
Sango <60422984+MatyilaSango@users.noreply.github.com>
2023-02-02 15:04:45 +0200

    Added custom useState hook;

--- testing.js
+++ testing.js
@@ -1,3 +1,29 @@
 let myname = "sango"
 const regex = "/^[a-zA-Z]+$/"
 console.log(regex.test(myname))
+
+
+const useSemiPersistentState = (key, initialState) => {
+
+  const isMounted = React.useRef(false);
+
+
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+
+    if (!isMounted.current) {
+      isMounted.current = true;
+    } else {
+
+      console.log('A');
+      localStorage.setItem(key, value);
+
+    }
+
+  }, [value, key]);
+
+  return [value, setValue];
+};
